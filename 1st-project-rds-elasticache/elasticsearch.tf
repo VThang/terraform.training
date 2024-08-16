f4379c253b508ef180e83541937a03281c7e26a6
VThang <leejaewoon9306@gmail.com>
2024-08-16 15:04:41 +0700

    ✨ Add Elasticache subnet group.

--- 1st-project-rds-elasticache/elasticsearch.tf
+++ 1st-project-rds-elasticache/elasticsearch.tf
@@ -1,1 +1,15 @@
 # Create Elasticache instance
+resource "aws_elasticache_subnet_group" "devops-test-1stproject-elasticache-subnet-group" {
+  name = "devops-test-1stproject-elasticache-subnet-group"
+  subnet_ids = [
+    aws_subnet.devops-test-1stproject-subnet-private-1.id,
+    aws_subnet.devops-test-1stproject-subnet-private-2.id
+  ]
+  depends_on = [
+    aws_subnet.devops-test-1stproject-subnet-private-1,
+    aws_subnet.devops-test-1stproject-subnet-private-2
+  ]
+  tags = {
+    "Name" = "${var.project-prefix}elasticache-subnet-group"
+  }
+}
