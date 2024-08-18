1e2684b0573dec7a97793e97e84e0f3b1ac4ef08
VThang <leejaewoon9306@gmail.com>
2024-08-18 19:29:56 +0700

    ✨ Add AWSCC providers

--- 1st-project-rds-elasticache/config.tf
+++ 1st-project-rds-elasticache/config.tf
@@ -4,9 +4,17 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 5.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = ">= 4.0.4"
+    # tls = {
+    #   source  = "hashicorp/tls"
+    #   version = ">= 4.0.4"
+    # }
+    awscc = {
+      source  = "hashicorp/awscc"
+      version = ">= 0.25.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.2"
     }
   }
 
