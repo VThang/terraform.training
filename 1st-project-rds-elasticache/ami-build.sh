b41b84e1c886c2eedfd26fc166d1f07b4e4e1811
vthang <leejaewoon9306@gmail.com>
2024-08-21 10:37:45 +0700

    🚀 Add AMI script

---
+++ 1st-project-rds-elasticache/ami-build.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+cd ami/ami-machine || exit
+echo "############################################"
+echo "# Creating Base instance for AMI creation  #"
+echo "############################################"
+terraform apply -auto-approve
+if [[ $? -eq 0 ]]; then
+	echo "##################################################"
+	echo "# Wait 180s for Base instance setup is completed #"
+	echo "##################################################"
+	secs=$((3 * 60))
+	while [[ $secs -gt 0 ]]; do
+		echo -ne "$secs\033[0K\r"
+		sleep 1
+		: $((secs--))
+	done
+
+	cd ../ami-creation || exit
+	echo "#########################################"
+	echo "# Creating AMI image from Base instance #"
+	echo "#########################################"
+	terraform apply -auto-approve
+
+	cd ../# ami-machine || exit
+	echo "####################################################"
+	echo "# Destroy Base instance after AMI image is created #"
+	echo "####################################################"
+	terraform destroy -auto-approve
+else
+	echo "#####################################"
+	echo "# Destroy AMI machine because Error #"
+	echo "#####################################"
+	terraform destroy -auto-approve
+fi
---
+++ 1st-project-rds-elasticache/ami-destroy.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd ami/ami-creation || exit
+echo "############################################"
+echo "Destroy AMI Images on AWS"
+echo "############################################"
+terraform destroy -auto-approve -refresh=false
